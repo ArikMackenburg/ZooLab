64d29e1658e7c39751c88eea820175c89ff8b001
ArikMackenburg <Arikmackenburg@gmail.com>
2020-08-31 23:40:37 -0500

    lab complete maybe?

---
+++ Zoo.Test/Zoo.Test.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Zoo.Animals;
+using Xunit;
+
+namespace Zoo.Test
+{
+    public class ZooTest
+    {
+        [Fact]
+        public static void FennecFox_Is_Cute()
+        {
+            FennecFox fox = new FennecFox();
+            fox.IsCute = true;
+            Assert.True(fox.IsCute);
+        }
+
+        [Fact]
+        public static void BaldEagle_reppin_Murrica()
+        {
+            BaldEagle baldy = new BaldEagle();
+            baldy.Name = "Baldy";
+            Assert.Equal("Murica!", baldy.Climate());
+            Assert.Equal("Baldy", baldy.Name);
+            Assert.False(baldy.ColdBlooded);
+        }
+        [Fact]
+        public static void Platypus_name_is_Perry()
+        {
+            DuckBilledPlatypus perry = new DuckBilledPlatypus();
+            Assert.Equal("Perry",perry.Name);
+            Assert.False(perry.IsCute);
+        }
+        [Fact]
+        public static void SeaTurtle_isnt_venomous()
+        {
+            SeaTurtle squirt = new SeaTurtle();
+            Assert.Equal("Squirt", squirt.Name);
+            Assert.False(squirt.Venomous);
+        }
+        [Fact]
+        
+        public static void Check_Origin()
+        {
+            BaldEagle baldy = new BaldEagle();
+            SeaTurtle squirt = new SeaTurtle();
+            Assert.Equal("I'm an animal", baldy.Animal());
+            Assert.Equal("I'm a cartoon", squirt.Animal());
+        }
+    }
+}
---
+++ Zoo/Animals/Mammals.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Zoo.Animals
+{
+    public abstract class Mammal : Animals
+    {
+        public override bool ColdBlooded => false;
+
+
+    }
+
+    public class FennecFox : Mammal
+    {
+        public override decimal WeightPounds => 2.2m;
+
+        public override string Sound()
+        {
+            return "Fox Noises";
+        }
+        public override string Eat()
+        {
+            return "Omnivore";
+        }
+        public override string Climate()
+        {
+            return "Desert";
+        }
+    }
+
+    public class DuckBilledPlatypus : Mammal
+    {
+        public DuckBilledPlatypus()
+        {
+            Name = "Perry";
+        }        
+        public override decimal WeightPounds => 3m;
+
+        public override string Sound()
+        {
+            return "My name is Perry";
+        }
+        public override string Eat()
+        {
+            return "Carnivore";
+        }
+        public override string Climate()
+        {
+            return "Australia";
+        }
+        public override string Animal()
+        {
+            return "I'm a cartoon";
+        }
+    }
+}
